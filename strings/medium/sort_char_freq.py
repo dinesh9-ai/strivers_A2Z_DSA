1de884d8daf0a6940065b512f68721603c773ff5
Dineshwar Doddapaneni <63300423+dinesh9-ai@users.noreply.github.com>
2022-09-18 19:44:42 +0530

    Create sort_char_freq.py

---
+++ strings/medium/sort_char_freq.py
@@ -0,0 +1,13 @@
+def frequencySort(a):
+        d={}
+        for i in a:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+        q=sorted(d,key=lambda x:d[x])
+        #print(d)
+        s=''
+        for i in q[::-1]:
+            s=s+(i)*d[i]
+        return s
