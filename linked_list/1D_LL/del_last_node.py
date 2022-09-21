06a4482e444452055cab73da1851f1fa183a1f1a
Dineshwar Doddapaneni <63300423+dinesh9-ai@users.noreply.github.com>
2022-09-22 00:32:02 +0530

    added delLastNode

--- linked_list/1D_LL/del_last_node.py
+++ linked_list/1D_LL/del_last_node.py
@@ -7,10 +7,8 @@ class LL:
     def __init__(self,head=None):
         self.head=head
 
+        
     def insert_node(self,x):
-        """
-        Code to add node at end
-        """
         if not self.head:
             self.head=node(x)
             return
@@ -20,15 +18,27 @@ class LL:
         h.nxt=node(x)
 
         
+    def delLast(self):
+        """
+        Code to delete last node of Linkend List
+        """
+        h=self.head
+        while h.nxt.nxt:
+            h=h.nxt
+        del h.nxt
+        h.nxt=None
     def Print(self):
         h=self.head
         while h:
             print(h.val,end=' -> ')
             h=h.nxt
         print('NULL')
+    
 
 a=[10,20,40,80,6]
 ll=LL()
 for i in a:
     ll.insert_node(i)
+ll.Print()
+ll.delLast()
 ll.Print()
