272f1f6da6bfd8f182431e62df356b45003bfcf7
Dineshwar Doddapaneni <63300423+dinesh9-ai@users.noreply.github.com>
2022-11-19 23:31:36 +0530

    read

--- recursion/subsequence_patterns/binary_strings.py
+++ recursion/subsequence_patterns/binary_strings.py
@@ -1,22 +1,16 @@
 k=int(input())
-s=[0]*k
+s=[None]*k
 
 def gen(k,s,n):
 
     if n==k:
         print(*s)
         return 
-    if s[n-1]==1:
-        s[n]=0
-        gen(k,s,n+1)
-    else:
-        s[n]=0
-        gen(k,s,n+1)
-        s[n]=1
-        gen(k,s,n+1)
+    
+    s[n]=0
+    gen(k,s,n+1)
+    s[n]=1
+    gen(k,s,n+1)
 
 
-s[0]=0
-gen(k,s,1)
-s[1]=1
-gen(k,s,1)
+gen(k,s,0)
